d4a50d2e687f65016887ed1c1e4b84ecc01a6451
Srun <zhoucan007@me.com>
2020-07-10 09:13:31 +0800

    Initial Commit

---
+++ MacChuiChui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacChuiChui
+//
+//  Created by Srun Zhou on 2020/7/10.
+//  Copyright © 2020 Srun Zhou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
